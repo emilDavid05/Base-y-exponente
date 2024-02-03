597ee93f7be427a89a7cd167d2cadaac5299658e
Laptop <Laptop@DESKTOP-AAIMPLJ>
2024-02-03 18:29:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la base:");
+        double basePotencia = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el exponente:");
+        double exponentePotencia = Convert.ToDouble(Console.ReadLine());
+
+        double resultadoPotencia = ElevarPotencia(basePotencia, exponentePotencia);
+
+        Console.WriteLine("El resultado es: " + resultadoPotencia);
+    }
+
+    static double ElevarPotencia(double basePotencia, double exponentePotencia)
+    {
+        return Math.Pow(basePotencia, exponentePotencia);
+    }
+}
